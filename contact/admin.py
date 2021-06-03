ff69ac6d533d428cf206e53072d1f7a6f442fe00
Elsherbeny <84872327+islamelsherbeny@users.noreply.github.com>
2021-06-03 14:39:27 +0200

    contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Info
 
 # Register your models here.
+
+#this is to have this changes in the admin panel
+admin.site.register(Info)
--- project/settings.py
+++ project/settings.py
@@ -41,10 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     #my apps
-    
     'job',
     'bootstrap4',
     'django_filters',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +144,10 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'islamsherbeny94@gmail.com'
+EMAIL_HOST_PASSWORD = 'embxfhjgsfyprpnx'
+EMAIL_USE_TLS = True
+EMAIL_PORT = '587'
